d2f48d389a5a9f704f5be56e5b16b7bb44ded507
IanNdiba <ianndiba@gmail.com>
2024-05-03 22:18:24 +0300

    commit

--- z-testing/app.js
+++ z-testing/app.js
@@ -1,24 +1,24 @@
-const houseForSale = new Map();
-houseForSale.set("area", 940);
-houseForSale.set("value", 320000);
-houseForSale.set("streetName", "Fifth Street");
-houseForSale.set("built", 2012);
-houseForSale.set("owner", { name: "Blake", age: 29 });
-houseForSale.set("offers", [290000, 295000, 315000, 312000]);
+console.log("Program Started");
 
-console.log(houseForSale);
-console.log("==============================================================");
-houseForSale.delete("built");
-console.log(houseForSale);
-console.log("==============================================================");
-houseForSale.get("owner").age = 30;
-console.log(houseForSale.get("owner"));
-console.log("==============================================================");
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ data: "Hello", error: null });
+  }, 5000);
+});
 
-const maxOfferPrice = houseForSale.get("offers").reduce((acc, offer) => {
-  return Math.max(acc, offer);
-}, 0);
-console.log(maxOfferPrice);
-console.log("==============================================================");
-houseForSale.set("sale price", 312000);
-console.log(houseForSale);
+console.log(myPromise);
+console.log("Program in progress...");
+
+myPromise
+  .then((value) => {
+    console.log(value);
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("First promise chain completed");
+      }, 2000);
+    });
+  })
+  .then((value) => {
+    console.log(value);
+  });
